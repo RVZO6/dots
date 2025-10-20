4a5a612fff46ead3452c886d0a0c5be56447ea8f
Ryan B <ryanbmb@proton.me>
2025-10-20 13:04:45 -0400

    Refactor nushell config with OS-specific symlinks

--- dot_config/nvim/lua/plugins/*lualine.lua
+++ dot_config/nvim/lua/plugins/*lualine.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     options = {
       icons_enabled = false,
-      theme = "mellow",
+      theme = "auto",
       component_separators = "|",
       section_separators = "",
     },
