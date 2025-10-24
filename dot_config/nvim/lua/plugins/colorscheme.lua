153b50d7d537f818cf75a570f80b2a0b94f61627
RVZO6 <rvzo6@users.noreply.github.com>
2025-10-24 16:49:10 -0400

    added undercurl

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,32 @@
 return {
-  {
-    "mellow-theme/mellow.nvim",
-    config = function()
-      vim.g.mellow_transparent = true
-      vim.cmd([[colorscheme Mellow]])
-      vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
-      vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
-    end,
-  },
-  -- NOTE: fuck
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "mellow",
-  --   },
-  -- },
+	{
+		"mellow-theme/mellow.nvim",
+		config = function()
+			vim.g.mellow_transparent = true
+			vim.cmd([[colorscheme Mellow]])
+			vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+			vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+			for _, group in ipairs({
+				"SpellBad",
+				"SpellCap",
+				"SpellRare",
+				"SpellLocal",
+				"DiagnosticUnderlineError",
+				"DiagnosticUnderlineWarn",
+				"DiagnosticUnderlineInfo",
+				"DiagnosticUnderlineHint",
+				"Underlined",
+			}) do
+				local current = vim.api.nvim_get_hl(0, { name = group })
+				vim.api.nvim_set_hl(0, group, vim.tbl_extend("force", current, { undercurl = true }))
+			end
+		end,
+	},
+	-- NOTE: fuck
+	-- {
+	--   "LazyVim/LazyVim",
+	--   opts = {
+	--     colorscheme = "mellow",
+	--   },
+	-- },
 }
