9b4b33ba8f9c970a357bbd6ff6c79cbc4631d800
RVZO6 <rvzo6@users.noreply.github.com>
2026-01-13 14:58:01 -0500

    the great sesh migration

--- dot_config/nvim/plugin/markdown.lua
+++ dot_config/nvim/plugin/markdown.lua
@@ -3,10 +3,15 @@ Config.later(function()
   vim.pack.add({ 'https://github.com/bullets-vim/bullets.vim',
     'https://github.com/MeanderingProgrammer/render-markdown.nvim' }, { load = true })
 
+  -- pcall(function()
+  --   vim.treesitter.language.register('markdown', 'markdown.chezmoitmpl')
+  -- end)
+
   require("render-markdown").setup({
-		latex = {
-			enabled = true
-		},
+    file_types = { "markdown", "markdown.chezmoitmpl" },
+    latex = {
+      enabled = true,
+    },
     code = {
       -- language_border = "",
       -- disable_background = true
