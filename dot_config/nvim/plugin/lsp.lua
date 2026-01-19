564361a136360d891c0e6d4be40c936d26156dbf
RVZO6 <rvzo6@users.noreply.github.com>
2026-01-19 15:23:08 -0500

    random shit

--- dot_config/nvim/plugin/lsp.lua
+++ dot_config/nvim/plugin/lsp.lua
@@ -3,7 +3,7 @@ Config.later(function()
   vim.pack.add({ 'https://github.com/neovim/nvim-lspconfig' }, { load = true })
 
   -- LSP servers to enable
-  local lsp_servers = { "lua_ls", "bashls", "jsonls", "clangd", "gopls", "basedpyright" }
+  local lsp_servers = { "lua_ls", "tailwindcss", "vtsls", "bashls", "jsonls", "clangd", "gopls", "basedpyright" }
 
   -- setup
   vim.lsp.enable(lsp_servers)
